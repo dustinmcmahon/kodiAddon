56c8c74cea39919471aa76a8c02b5f1e97d71356
Dustin McMahon <dpmcmahon85@gmail.com>
2023-03-26 22:30:30 -0400

    Initial GUI Window & logo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Change Log
 
+### 3/26/23
+- source code updated
+- renamed entry point to default.py
+- filled out the addon.xml document
+- created a window to display the UI in
+- added KES logo
+
 ### 2/20/2023
 - added uml diagram
   - drawio version
---
+++ Source/default.py
@@ -0,0 +1,30 @@
+import xbmcgui
+import xbmcaddon
+from resources import random_movie_player
+from resources.random_movie_player import play_random_movie
+
+addon = xbmcaddon.Addon()
+
+
+class MyPlayer(xbmcgui.Window):
+    def __init__(self):
+        xbmcgui.Window.__init__(self)
+
+        self.button = xbmcgui.ControlButton(
+            50, 50, 400, 100, label="Play Random Movie", focusTexture="special://home/addons/script.random_movie_player/resources/skins/default/media/focused.png")
+        self.addControl(self.button)
+
+    def onControl(self, control):
+        if control == self.button:
+            xbmcgui.Dialog().ok('addonname', 'This message')
+            # play_random_movie()
+
+    # def onClick(self, controlID: int):
+    #     if controlID == 10:
+    #         play_random_movie()
+
+
+if (__name__ == "__main__"):
+    w = MyPlayer()
+    w.doModal()
+    del w
