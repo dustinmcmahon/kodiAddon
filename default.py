8a3cd169520d38e3b7e3e91912c54ca84c7e2841
Queen-Mahella <113644654+Queen-Mahella@users.noreply.github.com>
2023-03-31 08:43:36 -0400

    Add files via upload

--- default.py
+++ default.py
@@ -1,31 +1,128 @@
-
 import xbmcgui
-import xbmcaddon
-from resources import random_movie_player
-from resources.random_movie_player import play_random_movie
 
-addon = xbmcaddon.Addon()
+#Try again later
+# dummy_list = [
 
+#         {'MediaType':'Movie', 'Name': 'Movie 1', 'Rating': '5', 'Genre': 'Horror', 'Length': '130', 'Year': '2019'},
+#         {'MediaType':'Epsiode', 'Name': 'Epsiode 2', 'Rating': '2', 'Genre': 'Slice-Of-Life', 'Length': '40', 'Year': '2020'},
+#         {'MediaType':'Movie', 'Name': 'Movie 2', 'Rating': '4', 'Genre': 'Comedy', 'Length': '90', 'Year': '2023'},
+#         {'MediaType':'Movie', 'Name': 'Movie 3', 'Rating': '3', 'Genre': 'Action', 'Length': '120', 'Year': '2022'},
+#         {'MediaType':'Episode', 'Name': 'Epsode 2', 'Rating': '1', 'Genre': 'Romance', 'Length': '20', 'Year': '2021'}
 
-class MyPlayer(xbmcgui.Window):
-    def __init__(self):
-        xbmcgui.Window.__init__(self)
-
-        self.button = xbmcgui.ControlButton(
-            50, 50, 400, 100, label="Play Random Movie", focusTexture="special://home/addons/script.random_movie_player/resources/skins/default/media/focused.png")
-        self.addControl(self.button)
-
-    def onControl(self, control):
-        if control == self.button:
-            xbmcgui.Dialog().ok('addonname', 'This message')
-            # play_random_movie()
-
-    # def onClick(self, controlID: int):
-    #     if controlID == 10:
-    #         play_random_movie()
+#         ]
 
 
-if (__name__ == "__main__"):
-    w = MyPlayer()
-    w.doModal()
-    del w
+class MyWindow(xbmcgui.Window):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+     #The Identifying stuff
+    def media_Type(self, text):
+        label = xbmcgui.ControlLabel(50, 50, 200, 100, text)
+        self.addControl(label)
+
+    def watch_Status(self, text):
+        label = xbmcgui.ControlLabel(190, 50, 200, 100, text)
+        self.addControl(label)
+
+    def rating(self, text):
+        label = xbmcgui.ControlLabel(370, 50, 200, 100, text)
+        self.addControl(label)
+
+    def genre(self, text):
+        label = xbmcgui.ControlLabel(520, 50, 200, 100, text)
+        self.addControl(label)
+
+    def length(self, text):
+        label = xbmcgui.ControlLabel(670, 50, 200, 100, text)
+        self.addControl(label)
+
+    def year(self, text):
+        label = xbmcgui.ControlLabel(830, 50, 200, 100, text)
+        self.addControl(label)
+
+    def tags(self, text):
+        label = xbmcgui.ControlLabel(980, 50, 200, 100, text)
+        self.addControl(label)
+
+    def studio(self, text):
+        label = xbmcgui.ControlLabel(1120, 50, 200, 100, text)
+        self.addControl(label)
+
+
+       #The Orange
+    def show_circle(self, x, y, radius, color):
+        circle = xbmcgui.ControlImage(x, y, radius, radius, "/Users/collegecarol/Desktop/Kodi/Kodi4/circle.png", color)
+        self.addControl(circle)
+
+    
+
+    # def get_names(items):
+    # list = []
+    # for item in items:
+    #     name = item.get('name', None)
+    #     if name is not None:
+    #         names.append(name)
+    # return names
+  
+    
+
+    #IDK
+    # names_only = get_names(dummy_list)
+    # dummy_list = create_dummy_list()
+
+    # first_params = []
+    # for param in dummy_list:
+    #     first_params.append(param[0])
+
+ 
+
+  #The Window
+if __name__ == "__main__":
+    
+    window = MyWindow()
+
+    #Orange
+    window.show_circle (30, 35, 150, 0xFF0000)
+    window.show_circle (180, 35, 150, 0xFF0000)
+    window.show_circle (330, 35, 150, 0xFF0000)
+    window.show_circle (480, 35, 150, 0xFF0000)
+    window.show_circle (630, 35, 150, 0xFF0000)
+    window.show_circle (780, 35, 150, 0xFF0000)
+    window.show_circle (930, 35, 150, 0xFF0000)
+    window.show_circle (1080, 35, 150, 0xFF0000)
+
+    window.show_circle (30, 185, 150, 0xFF0000)
+    window.show_circle (180, 185, 150, 0xFF0000)
+    window.show_circle (330, 185, 150, 0xFF0000)
+
+
+    window.show_circle (1080, 635, 200, 0xFF0000)
+    window.show_circle (880, 635, 200, 0xFF0000)
+    window.show_circle (680, 635, 200, 0xFF0000)
+    window.show_circle (480, 635, 200, 0xFF0000)
+
+    window.show_circle (35, 590, 300, 0xFF0000)
+
+
+    #Identifying
+    window.media_Type("MediaType")
+    window.watch_Status("WatchStatus")
+    window.rating("Rating")
+    window.genre("Genre")
+    window.length("Length")
+    window.year("Year")
+    window.tags("Tags")
+    window.studio("Studio")
+
+    #Try again Later
+    # button = window.getControl(200)
+    # button.setLabel("Display List")
+    # def display_list():
+    #     dialog = xbmcgui.Dialog()
+    #     dialog.ok("List", str(dummy_list))
+    # button.setNavigation( xbmcgui.ACTION_SELECT_ITEM, lambda: display_list() )
+
+    window.doModal()
+    del window
---
+++ metaData.py
@@ -0,0 +1,67 @@
+import sqlite3
+import os
+import xbmc
+import xbmcaddon
+
+########
+## This section doesnt work
+addon = xbmcaddon.Addon()
+if xbmc.getCondVisibility('system.platform.windows'):
+    videoDB = addon.getAddonInfo('path').replace("addons\\video.kodi.episode.selector\\", "userdata\\Database\\MyVideos121.db")
+if xbmc.getCondVisibility('system.platform.osx'):
+    videoDB = addon.getAddonInfo('path').replace("addons/video.kodi.episode.selector/", "userdata/Database/MyVideos121.db")
+
+def getMetaData(dataType):
+    connection = sqlite3.connect(videoDB)
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM {}".format(dataType)
+    cursor.execute(query)
+    returnList = cursor.fetchall()
+
+    connection.commit()
+    connection.close()
+    return returnList
+
+# unit test for functions
+def testMetaData():
+    print("Testing Actors: {}".format(getActors()))
+    print("Testing Directors: {}".format(getDirectors()))
+    print("Testing Genre: {}".format(getGenres()))
+    print("Testing Studio: {}".format(getStudios()))
+    print("Testing Tags: {}".format(getTags()))
+
+def getActors():
+    connection = sqlite3.connect(videoDB)
+    actors = connection.cursor().execute('SELECT actor_id, name FROM actor').fetchall()
+    connection.commit()
+    connection.close()
+    return actors
+
+def getDirectors():
+    connection = sqlite3.connect(videoDB)
+    directors = connection.cursor().execute('SELECT actor.actor_id,actor.name FROM director_link LEFT JOIN actor ON director_link.actor_id = actor.actor_id').fetchall()
+    connection.commit()
+    connection.close()
+    return directors
+
+def getGenres():
+    connection = sqlite3.connect(videoDB)
+    genres = connection.cursor().execute('SELECT * FROM genre').fetchall()
+    connection.commit()
+    connection.close()
+    return genres
+
+def getStudios():
+    connection = sqlite3.connect(videoDB)
+    studios = connection.cursor().execute('SELECT * FROM studio').fetchall()
+    connection.commit()
+    connection.close()
+    return studios
+
+def getTags():
+    connection = sqlite3.connect(videoDB)
+    tags = connection.cursor().execute('SELECT * FROM tag').fetchall()
+    connection.commit()
+    connection.close()
+    return tags
